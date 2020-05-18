014470940ebf1801537756a340ff3a90bdd79e0b
Lucas Fernandez Nicolau <dev.lucasfn@gmail.com>
2020-05-18 17:25:06 -0300

    TabBarController criado

---
+++ PresetsMarketplace/Controller/TabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarController.swift
+//  PresetsMarketplace
+//
+//  Created by Lucas Fernandez Nicolau on 18/05/20.
+//  Copyright © 2020 Lucas Fernandez Nicolau. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
