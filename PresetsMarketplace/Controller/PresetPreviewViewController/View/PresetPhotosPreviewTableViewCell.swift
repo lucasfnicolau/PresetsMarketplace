087369b177eabfd122976adab70e84f08fbe1041
Lucas Fernandez Nicolau <dev.lucasfn@gmail.com>
2020-05-19 18:26:12 -0300

    TableViewCell renomeada

--- PresetsMarketplace/Controller/PresetPreviewViewController/View/PresetPhotosPreviewTableViewCell.swift
+++ PresetsMarketplace/Controller/PresetPreviewViewController/View/PresetPhotosPreviewTableViewCell.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SlideToMoreInfoPresetTableViewCell: UITableViewCell {
+class PresetPhotosPreviewTableViewCell: UITableViewCell {
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var chevronImageView: UIImageView!
 
