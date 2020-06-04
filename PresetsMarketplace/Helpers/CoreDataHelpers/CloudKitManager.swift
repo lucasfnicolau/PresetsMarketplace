a59792f23e0fa66da975c008288426532d2c362a
Pedro Henrique Guedes Silveira <pedrohgsilveira@gmail.com>
2020-06-04 15:43:21 -0300

    Implement Core Data Helpers

---
+++ PresetsMarketplace/Helpers/CoreDataHelpers/CloudKitManager.swift
@@ -0,0 +1,9 @@
+//
+//  CloudKitManager.swift
+//  PresetsMarketplace
+//
+//  Created by Pedro Henrique Guedes Silveira on 04/06/20.
+//  Copyright © 2020 Lucas Fernandez Nicolau. All rights reserved.
+//
+
+import Foundation
